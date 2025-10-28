053e36530fff9f27b9ad7a5be753ddc09f579b97
Kamitet123 <nikityshka156@mail.ru>
2025-10-28 17:40:38 +0400

    Create niklay.cpp

---
+++ niklay.cpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef NIKLAY_H
+#define NIKLAY_H
+
+#include <string>
+
+class niklay_bomsh {
+  std::string name;
+public:
+  niklay_bomsh(const std::string n);
+  void HI();
+
+};
+
+
+#endif // !_NIKLAY_H
