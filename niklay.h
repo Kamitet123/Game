75ffbce795e306b584f83d3268cb5296df487bcc
Kamitet123 <nikityshka156@mail.ru>
2025-10-28 17:39:09 +0400

    Create niklay.h

---
+++ niklay.h
@@ -0,0 +1,12 @@
+#include "niklay.h"
+#include <iostream>
+
+niklay_bomsh::niklay_bomsh(const std::string n)
+{
+
+}
+
+void niklay_bomsh::HI()
+{
+  std::cout << "Привет, меня зовут Николай " << std::endl;
+}
